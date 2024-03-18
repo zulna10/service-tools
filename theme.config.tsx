debcd992f12783035c863498bcf32c78b261531b
zulna <nishwa100601@gmail.com>
2024-03-19 05:56:36 +0700

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -35,7 +35,7 @@ const config: DocsThemeConfig = {
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: (
       <span>
